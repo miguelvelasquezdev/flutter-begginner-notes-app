2544e0d7cb7abbbebbd85d6fa850c1cabf17e73e
Miguel Velasquez <veruiz.miguel@gmail.com>
2022-11-12 19:56:42 -0500

    step-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_course_for_beginners/constants/routes.dart';
 import 'package:flutter_course_for_beginners/services/auth/auth_service.dart';
